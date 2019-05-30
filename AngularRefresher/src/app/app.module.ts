2db3b0eb22fd258709675f886fe59f0587fd8c81
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2019-05-30 18:19:48 -0400

    Adding Components
    Create component
    ng g c product --spec=false

--- AngularRefresher/src/app/app.module.ts
+++ AngularRefresher/src/app/app.module.ts
@@ -2,15 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products.component';
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, ProductsComponent, ProductComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
