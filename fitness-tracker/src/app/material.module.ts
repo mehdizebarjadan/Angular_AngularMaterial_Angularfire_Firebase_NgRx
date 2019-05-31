5819bdb13345331f7983d18993988351c4dcd1f3
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2019-05-31 03:35:15 -0400

    Working on The Signup Form

--- fitness-tracker/src/app/material.module.ts
+++ fitness-tracker/src/app/material.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule, MatIconModule } from '@angular/material';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatIconModule],
-  exports: [MatButtonModule, MatIconModule]
+  imports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule],
+  exports: [MatButtonModule, MatIconModule, MatInputModule, MatFormFieldModule]
 })
 export class MaterialModule {}
