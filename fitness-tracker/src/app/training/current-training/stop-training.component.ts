6dd8b3b11a5a7e23baece40dbe8885af8a21b9f4
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2019-06-01 10:54:34 -0400

    Passing Data to the Dialog

--- fitness-tracker/src/app/training/current-training/stop-training.component.ts
+++ fitness-tracker/src/app/training/current-training/stop-training.component.ts
@@ -1,13 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-stop-training',
-  template: `
-    <h1 mat-dialog-title>Are you sure?</h1>
-    <mat-dialog-actions>
-      <button mat-button [mat-dialog-close]="true">Yes</button>
-      <button mat-button [mat-dialog-close]="false">No</button>
-    </mat-dialog-actions>
-  `
+  templateUrl: './stop-training.component.html'
 })
-export class StopTrainingComponent {}
+export class StopTrainingComponent {
+  constructor(@Inject(MAT_DIALOG_DATA) public passedData: any) {}
+}
