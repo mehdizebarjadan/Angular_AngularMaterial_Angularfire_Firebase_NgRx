16d7d195ca04a164be228ef6eb919509d4923f4e
mehdizebarjadan <mehdizebarjadan@yahoo.com>
2019-06-05 06:28:01 -0400

    Operators & RxJS 6

--- fitness-tracker/src/app/training/new-training/new-training.component.ts
+++ fitness-tracker/src/app/training/new-training/new-training.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import 'rxjs/add/operator/map';
 
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
@@ -12,7 +14,7 @@ import { Exercise } from '../exercise.model';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any>;
+  exercises: Observable<Exercise[]>;
 
   constructor(
     private trainingService: TrainingService,
@@ -21,10 +23,74 @@ export class NewTrainingComponent implements OnInit {
 
   ngOnInit() {
     // this.exercises = this.trainingService.getAvailableExercises();
-    this.exercises = this.db.collection('availableExercises').valueChanges();
+    this.exercises = this.db
+      .collection('availableExercises')
+      .snapshotChanges()
+      .map(docArray => {
+        return docArray.map(doc => {
+          return {
+            id: doc.payload.doc.id,
+            name: doc.payload.doc.data().name,
+            duration: doc.payload.doc.data().duration,
+            calories: doc.payload.doc.data().calories
+          };
+        });
+      });
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
   }
 }
+
+// ngOnInit() {
+//   // this.exercises = this.trainingService.getAvailableExercises();
+//   this.exercises = this.db
+//     .collection('availableExercises')
+//     .snapshotChanges()
+//     .map(docArray => {
+//       return docArray.map(doc => {
+//         return {
+//           id: doc.payload.doc.id,
+//           name: doc.payload.doc.data().[name],
+//           duration: doc.payload.doc.data().[duration],
+//           calories: doc.payload.doc.data().[calories]
+//         };
+//       });
+//     });
+// }
+
+// ngOnInit() {
+//   // this.exercises = this.trainingService.getAvailableExercises();
+//   this.db
+//     .collection('availableExercises')
+//     .snapshotChanges()
+//     .map(docArray => {
+//       return docArray.map(doc => {
+//         return {
+//           id: doc.payload.doc.id,
+//           ...doc.payload.doc.data()
+//         };
+//       });
+//     })
+//     .subscribe(result => {
+//       console.log(result);
+//     });
+// }
+
+// ngOnInit() {
+//   // this.exercises = this.trainingService.getAvailableExercises();
+//   this.db
+//     .collection('availableExercises')
+//     .snapshotChanges()
+//     .subscribe(result => {
+//       for (const res of result) {
+//         console.log(
+//           res.payload.doc.id,
+//           res.payload.doc.data().name,
+//           res.payload.doc.data().duration,
+//           res.payload.doc.data().calories
+//         );
+//       }
+//     });
+// }
